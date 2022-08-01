832864c1a931c9b344756bd7fa0563b86d5e0057
Prateek Bansal <prateekbansalind@gmail.com>
2022-08-01 20:23:27 +1200

    add need for generics first exercise

---
+++ 12-generics-need-for-generics/src/main/java/org/pbansal/Main.java
@@ -0,0 +1,17 @@
+package org.pbansal;
+
+import org.pbansal.generics.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List list = new List();
+        list.add(1);
+
+
+    }
+}
+
+// generics first exercise
+// we have created one List class to hold an array of items whose maximum size is 10.
+
